bd630c42115f48fe5ef17fa6220e191038bcca63
Chris <chrisanben@gmail.com>
2013-07-08 22:57:11 -0400

    RashRoad v.0.2

--- RashRoad.py
+++ RashRoad.py
@@ -7,13 +7,16 @@
                         in which the players car tries to avoid incoming traffic while
                         trying to score points.
                         
-    VERSION 0.1: In the Works
-                    - Created RoadRash using MailPilot, credit to Tom for the original file.
-                    - Changed orientation of the car.
+    VERSION 0.2: Outgoing Cars and Road
+                    - Using a bit of Mailpilot I created a moving background for the road.
+                    - Changed the sprites for the cars
+                    - Added an outgoing car which the player can collide with
     
 """
-import pygame
+import pygame, random
 pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
 
 class PlayerCar(pygame.sprite.Sprite):
     def __init__(self):
@@ -27,17 +30,59 @@ class PlayerCar(pygame.sprite.Sprite):
     def update(self):
         mousex, mousey = pygame.mouse.get_pos()
         self.rect.center = (80, mousey)
+
+class OutCar(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/car.png")
+        self.image = self.image.convert()
+        self.transColor = self.image.get_at((1, 1))
+        self.image.set_colorkey(self.transColor)
+        self.rect = self.image.get_rect()
+        self.reset()
+        
+        self.dx = 4
+    
+    def update(self):
+        self.rect.centerx -= self.dx
+        if self.rect.right < 0:
+            self.reset()
+            
+    def reset(self):
+        self.rect.left = 640
+        self.rect.centery = (random.randrange(0,3)*80) + 265
+
+class Road(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/road.png")
+        self.image = self.image.convert()
+        self.image = pygame.transform.scale(self.image, (1280, 480))
+        self.rect = self.image.get_rect()
+        self.dx = 10
+        self.reset()
+        
+    def update(self):
+        self.rect.right -= self.dx
+        print(self.rect.left, self.rect.right)
+        if self.rect.left <= -640:
+            self.reset()
+    
+    def reset(self):
+        self.rect.left = 0
         
 def main():
-    screen = pygame.display.set_mode((640, 480))
-    pygame.display.set_caption("Mail Pilot! mpPlane.py - creating the Plane sprite")
+    pygame.display.set_caption("RoadRash v0.2")
 
     background = pygame.Surface(screen.get_size())
-    background.fill((0, 0, 255))
+    background.fill((0, 0, 0))
     screen.blit(background, (0, 0))
-    car = PlayerCar()
+
+    pcar = PlayerCar()
+    car = OutCar()
+    road = Road()
     
-    allSprites = pygame.sprite.Group(car)
+    allSprites = pygame.sprite.OrderedUpdates(road, pcar, car)
     clock = pygame.time.Clock()
     keepGoing = True
     while keepGoing:
@@ -46,8 +91,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 keepGoing = False
+
+        if pcar.rect.colliderect(car.rect):
+            car.reset()
                 
-        allSprites.clear(screen, background)
         allSprites.update()
         allSprites.draw(screen)
         
